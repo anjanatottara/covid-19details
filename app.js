6e3602d52f7e87d1c6b08ecdd877ff6f96984cc0
anjanatottara <anju951998@gamail.com>
2023-12-01 04:56:20 +0000

    solution6a

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.post("/districts/", async (request, response) => {
   const { districtName, stateId, cases, cured, active, deaths } = request.body;
   const postDistrictQuery = `INSERT INTO district (district_name,state_id,cases,cured,active,deaths)
      VALUES ('${districtName}',${stateId},${cases},${cured},${active},${deaths});`;
-  await database.run(getDistrictQuery);
+  await database.run(postDistrictQuery);
   response.send("District Successfully Added");
 });
 app.get("/districts/:districtId/", async (request, response) => {
